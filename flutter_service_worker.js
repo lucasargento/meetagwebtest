449bf89841771f228cc9b04c6dfa3a6c758268e8
Lucas Argento <lucasargento@Lucass-MacBook-Air.local>
2021-11-01 19:25:06 -0300

    bug fixes + elimine los prints de debug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4bc3c2055c6c420db26d1b1ec57b04ae",
-"index.html": "2165cd09b6b41a39317e699e423580ba",
-"/": "2165cd09b6b41a39317e699e423580ba",
-"main.dart.js": "4f31abedbfc6b84f9edf97239abaddff",
+"index.html": "e6b4743480f3828666c5ef900aef1c70",
+"/": "e6b4743480f3828666c5ef900aef1c70",
+"main.dart.js": "a649ba3992b9262de85e78410f0a751c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
